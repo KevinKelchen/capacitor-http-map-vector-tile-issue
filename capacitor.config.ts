80d6e96801ba0fde785eb6ec56a6d8f8e1958111
Kevin Kelchen <kelchen@cartegraph.com>
2022-11-28 19:14:05 -0600

    Rename app.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'capacitor-http-url-spaces',
+  appName: 'capacitor-http-wasm-issue',
   webDir: 'www',
   bundledWebRuntime: false,
   plugins: {
