573a81328f771c9a52c23f6a1aed87f2498c438b
Kevin Kelchen <kelchen@cartegraph.com>
2022-11-30 16:01:36 -0600

    Add repro.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,9 +1,8 @@
-import { HttpClient } from "@angular/common/http";
-import { Component } from '@angular/core';
-import { firstValueFrom } from "rxjs";
-import { EsriLoaderService } from "../esri-loader.service";
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { EsriLoaderService } from '../esri-loader.service';
 
-declare type EsriProjectionType = typeof import('esri/geometry/projection');
+declare type EsriVectorTileLayerType = typeof import('esri/layers/VectorTileLayer');
+declare type EsriMapType = typeof import('esri/map');
 
 @Component({
   selector: 'app-home',
@@ -11,19 +10,35 @@ declare type EsriProjectionType = typeof import('esri/geometry/projection');
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild('map', { read: ElementRef }) mapElRef: ElementRef | undefined;
 
-  esriProjection: EsriProjectionType | undefined;
+  esriMap: EsriMapType | undefined;
+  esriVectorTileLayer: EsriVectorTileLayerType | undefined;
 
   constructor(
-    private httpClient: HttpClient,
-    private esriLoaderService: EsriLoaderService,
+    private esriLoaderService: EsriLoaderService
   ) {}
 
-  async loadWasm() {
-    [this.esriProjection] = await this.esriLoaderService.loadModules([
-      'esri/geometry/projection'
-    ]);
+  async loadMap() {
+    // Using sample from https://developers.arcgis.com/javascript/3/jssamples/layers_vector.html
+    this.esriLoaderService.loadCss();
 
-    await this.esriProjection!.load();
+    [this.esriMap, this.esriVectorTileLayer] =
+      await this.esriLoaderService.loadModules([
+        'esri/map',
+        'esri/layers/VectorTileLayer',
+      ]);
+
+    const map = new this.esriMap!(this.mapElRef?.nativeElement, {
+      center: [2.3508, 48.8567], // longitude, latitude
+      zoom: 2,
+    });
+
+    //The URL referenced in the constructor may point to a style url JSON (as in this sample)
+    //or directly to a vector tile service
+    var vtlayer = new this.esriVectorTileLayer!(
+      'https://www.arcgis.com/sharing/rest/content/items/4cf7e1fb9f254dcda9c8fbadb15cf0f8/resources/styles/root.json'
+    );
+    map.addLayer(vtlayer);
   }
 }
